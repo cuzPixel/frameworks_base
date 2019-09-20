c0bccdebb9835311d1e8ecf566d19d49ab74db18
Lucchetto <lucchetto.tie@live.com>
2019-09-20 12:16:18 +0200

    [1/2] base: Double tap to sleep on lockscreen
    Change-Id: Icd8de7c5512362f22dcea9c3946347a1f0c13aba

--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -86,5 +86,6 @@ public class SystemSettings {
         Settings.System.NOTIFICATION_LIGHT_PULSE,
         Settings.System.LOCKSCREEN_MEDIA_METADATA,
         Settings.System.DOUBLE_TAP_SLEEP_GESTURE,
+        Settings.System.DOUBLE_TAP_SLEEP_LOCKSCREEN,
     };
 }
