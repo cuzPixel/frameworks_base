88ef73966582124eda965f39bbbe8d59e80b0539
Demon000 <demonsingur@gmail.com>
2020-04-06 15:47:43 +0200

    DozeSensors: only use proximity sensor if supported
    On msm-4.14 devices, when the proximity sensor is in use,
    the smp2p-sleepstate IRQ is fired multiple times a second,
    with each one holding a 200ms wakelock.
    This is probably a bug in the DSP firmware.
    To fix this, avoid using the proximity sensor in doze mode,
    because sleep is preferred to turning off the screen.
    
    Change-Id: I57750afd77267abdc49780f70636626d20e666ad

--- packages/SystemUI/src/com/android/systemui/doze/DozeSensors.java
+++ packages/SystemUI/src/com/android/systemui/doze/DozeSensors.java
@@ -51,6 +51,7 @@ import com.android.systemui.statusbar.phone.DozeParameters;
 import com.android.systemui.util.sensors.AsyncSensorManager;
 import com.android.systemui.util.sensors.ProximitySensor;
 import com.android.systemui.util.wakelock.WakeLock;
+import com.android.systemui.R;
 
 import java.io.PrintWriter;
 import java.util.Collection;
@@ -174,13 +175,15 @@ public class DozeSensors {
                         dozeLog),
         };
 
-        setProxListening(false);  // Don't immediately start listening when we register.
-        mProximitySensor.register(
-                proximityEvent -> {
-                    if (proximityEvent != null) {
-                        mProxCallback.accept(!proximityEvent.getBelow());
-                    }
-                });
+        if (context.getResources().getBoolean(R.bool.doze_proximity_sensor_supported)) {
+            setProxListening(false);  // Don't immediately start listening when we register.
+            mProximitySensor.register(
+                    proximityEvent -> {
+                        if (proximityEvent != null) {
+                            mProxCallback.accept(!proximityEvent.getBelow());
+                        }
+                    });
+        }
     }
 
     /**
