b14954ade40f9d4ee9ece1c49bc785df9ffcc064
raysenlau <raysen@live.cn>
2021-07-08 08:47:15 +0800

    Update fingerprints to sunfish Jul

--- core/java/com/android/internal/util/custom/PixelPropsUtils.java
+++ core/java/com/android/internal/util/custom/PixelPropsUtils.java
@@ -72,7 +72,7 @@ public class PixelPropsUtils {
         propsToChange.put("DEVICE", "sunfish");
         propsToChange.put("PRODUCT", "sunfish");
         propsToChange.put("MODEL", "Pixel 4a");
-        propsToChange.put("FINGERPRINT", "google/sunfish/sunfish:11/RQ3A.210605.005/7349499:user/release-keys");
+        propsToChange.put("FINGERPRINT", "google/sunfish/sunfish:11/RQ3A.210705.001/7380771:user/release-keys");
     }
 
     public static void setProps(String packageName) {
